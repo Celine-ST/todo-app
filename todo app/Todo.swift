71b6273f3b8355aa1774a9330fe42a4dc59d836b
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-07-29 11:31:25 +0800

    puuuuuuuuuusssssssssshhhhhhhhhhhh

---
+++ todo app/Todo.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  todo app
+//
+//  Created by Celine Quek on 29/7/23.
+//
+
+import Foundation
+
+struct Todo: Identifiable {
+    let id = UUID()
+    var title: String
+    var isCompleted = false
+}
