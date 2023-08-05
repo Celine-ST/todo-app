270592449fdbf356d58717d78cab77dd91c29772
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-08-05 11:39:00 +0800

    some samples if the user is lazy

---
+++ todo app/Model and ViewModel/Todo+Sample.swift
@@ -0,0 +1,18 @@
+//
+//  Todo+Sample.swift
+//  todo app
+//
+//  Created by Celine Quek on 5/8/23.
+//
+
+extension Todo {
+
+static let sampleTodos = [
+Todo(title: "Feed the cat", isCompleted: true),
+Todo(title: "Play with cat", subtitle: "Use his favourite String!"),
+Todo(title: "Get allergies"),
+Todo(title: "Run away from cat"),
+Todo(title: "Get a new cat")
+]
+
+}
